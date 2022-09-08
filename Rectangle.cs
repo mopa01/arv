dbb839739b7301a8d8f311265230935434ad71af
E-Svensson <111962443+E-Svensson@users.noreply.github.com>
2022-09-08 12:29:23 +0200

    52
    8

---
+++ Rectangle.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mopa
+{
+    class Rectangle:Shape
+    {
+        public Rectangle(int width, int height) : base( width, height)
+        {
+            Rectangle t= new Rectangle (25,15);
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+class Triangle : Shape
+{
+        public Triangle(int width, int height) : base( width, height)
+        {
+            Triangle t = new Triangle ()
+        }
+}
+
